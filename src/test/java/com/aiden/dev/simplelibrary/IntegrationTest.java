d0037b395b31c3a769578096a6bc695a53b0b029
Aiden <tyjk32@gmail.com>
2021-10-22 11:28:13 +0900

    Add test code

--- src/test/java/com/aiden/dev/simplelibrary/IntegrationTest.java
+++ src/test/java/com/aiden/dev/simplelibrary/IntegrationTest.java
@@ -1,8 +1,76 @@
 package com.aiden.dev.simplelibrary;
 
+import com.aiden.dev.simplelibrary.modules.account.AccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("통합 테스트")
 public class IntegrationTest {
 
+    @Autowired MockMvc mockMvc;
+    @Autowired AccountRepository accountRepository;
+
+    @DisplayName("index 페이지 테스트")
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 폼 테스트")
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("signUpForm"))
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 테스트 - 잘못된 입력값")
+    @Test
+    void signUp_wrong_value() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("loginId", "test")
+                        .param("password", "test1234")
+                        .param("passwordConfirm", "test5678")
+                        .param("nickname", "test")
+                        .param("email", "test@email.com")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("회원 가입 테스트")
+    @Test
+    void signUp() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("loginId", "test")
+                        .param("password", "test1234")
+                        .param("passwordConfirm", "test1234")
+                        .param("nickname", "test")
+                        .param("email", "test@email.com")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+
+        assertThat(accountRepository.findAll().size()).isEqualTo(1);
+    }
 }
